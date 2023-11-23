c37907ee9cf7fcab838791153718e214c4d5d42d
HeatherKolinskyBSU <146090360+HeatherKolinskyBSU@users.noreply.github.com>
2023-11-23 17:20:06 +0000

    Update Ch4-Ex1.cpp
    Includes the first four required points, with code annotations.
    Still trying to figure out how to shorten the horrendously long 'const char*' array.
    More commits to follow.

--- Ch4-Ex1.cpp
+++ Ch4-Ex1.cpp
@@ -5,8 +5,7 @@ using namespace std;
 
 int main() {    // Initialises the main function.
 	const char* alphabet[]{ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
-	// Creates an array storing the English alphabet
-	// as individual characters using 'const char*'.
+	// Creates an array storing the English alphabet as individual characters using 'const char*'.
 	// Surely there's a better way of storing this in plain code.
 	for (int j = 0; j < 26; j++) {
 		cout << alphabet[j] << endl;
@@ -21,6 +20,20 @@ int main() {    // Initialises the main function.
 	}
 	// Pretty much the same as the previous 'for' loop - however now the
 	// code is only outputting the second half of the English alphabet.
+
+	cout << endl;
+
+	int reverse[]{ 1, 3, 5, 7, 9 };
+	int size = sizeof(reverse) / sizeof(int);
+	// Creates a new array for storing individual numbers with 'int'.
+	// Assigns each value with a position number for later use.
+	for (int j = size-1; j >= 0; j--) {
+		cout << reverse[j] << endl;
+	}
+	// 'for' loop starts array at position 4 and decrements
+	// by 1 each time until it reaches position 0.
+
+	cout << endl;
 
 	return (0); // Returns function.
 }               // Ends the main function.
